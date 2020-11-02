f648821f5497832efe9a44b4995d1f8ecf58b2ae
Juliette117 <ajuliette7@gmail.com>
2020-11-02 20:58:40 +0100

    first commit

---
+++ CVLaetitia/index.js
@@ -0,0 +1,41 @@
+// alert("Hello world");
+
+ScrollReveal().reveal('.formations', {delay: 400, origin: 'top', distance: '40px'});
+ScrollReveal().reveal('.experiences', { delay: 800 });
+ScrollReveal().reveal('.compétences', { delay: 1200 });
+
+
+// const body = document.querySelector("body");
+// const swatch = document.querySelector(".swatch");
+// const range = document.querySelector(".range");
+
+// range.addEventListener("input", (e) => {
+// 	let val = e.target.value;
+
+// 	let hex = "#" + String(val).padStart(6, "0");
+
+// 	swatch.innerHTML = hex;
+// 	body.style.backgroundColor = hex;
+// });
+
+
+// import Darkmode from 'darkmode-js';
+
+// new Darkmode().showWidget();
+
+// const options = {
+//     bottom: '64px', // default: '32px'
+//     right: 'unset', // default: '32px'
+//     left: '32px', // default: 'unset'
+//     time: '0.5s', // default: '0.3s'
+//     mixColor: '#fff', // default: '#fff'
+//     backgroundColor: '#fff',  // default: '#fff'
+//     buttonColorDark: '#100f2c',  // default: '#100f2c'
+//     buttonColorLight: '#fff', // default: '#fff'
+//     saveInCookies: false, // default: true,
+//     label: '🌓', // default: ''
+//     autoMatchOsTheme: true // default: true
+//   }
+  
+//   const darkmode = new Darkmode(options);
+//   darkmode.showWidget();
