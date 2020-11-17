790affed330fa94ed354d9d8fbd062bdbfd918ae
Juliette117 <ajuliette7@gmail.com>
2020-11-17 18:59:31 +0100

    first commit

---
+++ blog mobile/App.js
@@ -0,0 +1,34 @@
+alert("Hello World");
+
+ScrollReveal().reveal('h1', {delay: 500, origin: 'top', distance: '40px'});
+ScrollReveal().reveal('h5', {delay: 500, origin: 'top', distance: '40px'});
+ScrollReveal().reveal('.card-text', {delay: 1000, origin: 'top', distance: '40px'});
+ScrollReveal().reveal('li', {delay: 500, origin: 'top', distance: '40px'});
+
+
+
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+}
+ 
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+
+// if (window.matchMedia("(min-width: 600px)").matches) {
+//
+//   } else {
+//    
+//   }
+
+
+
+
+
+
+
+
+
