16d66bd8f196c096d686fdf90f7707c7a1914f8c
Juliette117 <ajuliette7@gmail.com>
2020-11-18 18:28:52 +0100

    animations

--- blog mobile/App.js
+++ blog mobile/App.js
@@ -1,8 +1,10 @@
 
-ScrollReveal().reveal('h1', {delay: 500, origin: 'top', distance: '40px'});
-ScrollReveal().reveal('h5', {delay: 500, origin: 'top', distance: '40px'});
-ScrollReveal().reveal('.card-text', {delay: 1000, origin: 'top', distance: '40px'});
-ScrollReveal().reveal('li', {delay: 500, origin: 'top', distance: '40px'});
+ScrollReveal().reveal('h1', {delay: 100, origin: 'top', distance: '40px', scale: 2});
+ScrollReveal().reveal('h5', {delay: 900, origin: 'top', distance: '40px'});
+ScrollReveal().reveal('.card', {delay: 1200, origin: 'top', distance: '40px'});
+ScrollReveal().reveal('.card-text', {delay: 1200, origin: 'top', distance: '40px'});
+ScrollReveal().reveal('li', {delay: 700, origin: 'top', distance: '40px'});
+
 
 
 
@@ -15,6 +17,29 @@ function closeNav() {
     document.getElementById("sideNavigation").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+
+
+const titre = document.getElementById("titre", "texte");
+const txt = titre.dataset.label;
+let i 	= 0 ;
+function showLetters()
+{
+  let timeOut ;
+  if(i < txt.length)
+	{
+	  titre.innerHTML += `<span>${txt[i]}</span>` ;
+	  timeOut = setTimeout(showLetters,70)
+	  i++
+	}
+
+}
+showLetters();
+
+
+
+
+
 
 
 // if (window.matchMedia("(min-width: 600px)").matches) {
