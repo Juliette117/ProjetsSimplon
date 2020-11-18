5f91a23a4bc7a580e5162a44e70d774b02ff2e66
Juliette117 <ajuliette7@gmail.com>
2020-11-18 15:31:13 +0100

    responsive

--- CVLaetitia/index.js
+++ CVLaetitia/index.js
@@ -1,44 +1,52 @@
-// // alert("Hello world");
 
-// ScrollReveal().reveal('.formations', {delay: 400, origin: 'top', distance: '40px'});
-// ScrollReveal().reveal('.experiences', { delay: 800 });
+
+ScrollReveal().reveal('h2', {delay: 200, origin: 'top', distance: '40px'});
+ScrollReveal().reveal('a', {delay: 300, origin: 'top', distance: '40px'});
+
+
+
+
+
+// ScrollReveal().reveal('.experiences', { delay: 1200 });
 // ScrollReveal().reveal('.compétences', { delay: 1200 });
 
 
-// // const body = document.querySelector("body");
-// // const swatch = document.querySelector(".swatch");
-// // const range = document.querySelector(".range");
-
-// // range.addEventListener("input", (e) => {
-// // 	let val = e.target.value;
-
-// // 	let hex = "#" + String(val).padStart(6, "0");
-
-// // 	swatch.innerHTML = hex;
-// // 	body.style.backgroundColor = hex;
-// // });
 
 
-// // import Darkmode from 'darkmode-js';
+// const body = document.querySelector("body");
+// const swatch = document.querySelector(".swatch");
+// const range = document.querySelector(".range");
 
-// // new Darkmode().showWidget();
+// range.addEventListener("input", (e) => {
+// 	let val = e.target.value;
 
-// // const options = {
-// //     bottom: '64px', // default: '32px'
-// //     right: 'unset', // default: '32px'
-// //     left: '32px', // default: 'unset'
-// //     time: '0.5s', // default: '0.3s'
-// //     mixColor: '#fff', // default: '#fff'
-// //     backgroundColor: '#fff',  // default: '#fff'
-// //     buttonColorDark: '#100f2c',  // default: '#100f2c'
-// //     buttonColorLight: '#fff', // default: '#fff'
-// //     saveInCookies: false, // default: true,
-// //     label: '🌓', // default: ''
-// //     autoMatchOsTheme: true // default: true
-// //   }
+// 	let hex = "#" + String(val).padStart(6, "0");
+
+// 	swatch.innerHTML = hex;
+// 	body.style.backgroundColor = hex;
+// });
+
+
+// import Darkmode from 'darkmode-js';
+
+// new Darkmode().showWidget();
+
+// const options = {
+//     bottom: '64px', // default: '32px'
+//     right: 'unset', // default: '32px'
+//     left: '32px', // default: 'unset'
+//     time: '0.5s', // default: '0.3s'
+//     mixColor: '#fff', // default: '#fff'
+//     backgroundColor: '#fff',  // default: '#fff'
+//     buttonColorDark: '#100f2c',  // default: '#100f2c'
+//     buttonColorLight: '#fff', // default: '#fff'
+//     saveInCookies: false, // default: true,
+//     label: '🌓', // default: ''
+//     autoMatchOsTheme: true // default: true
+//   }
   
-// //   const darkmode = new Darkmode(options);
-// //   darkmode.showWidget();
+//   const darkmode = new Darkmode(options);
+//   darkmode.showWidget();
 
 
 
