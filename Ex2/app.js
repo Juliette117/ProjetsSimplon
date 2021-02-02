124d4de00b48394141bae9039a55e3df6d4c7f52
Juliette <ajuliette7@gmail.com>
2021-02-02 11:02:38 +0100

    ajout selecteur

--- Ex2/app.js
+++ Ex2/app.js
@@ -1,4 +1,14 @@
 // alert("hello world");
+
+
+
+const colorInput = document.querySelector('#choose');
+
+colorInput.addEventListener('change', function () {
+    document.querySelector('body').style.backgroundColor = colorInput.value;
+});
+
+
 function changeColor(color) { 
     document.body.style.background = color; 
 }
