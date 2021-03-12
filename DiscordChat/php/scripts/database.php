be64246c5165703b98784af058708dc497314941
Juliette <ajuliette7@gmail.com>
2021-03-12 20:47:52 +0100

    nopassword

--- DiscordChat/php/scripts/database.php
+++ DiscordChat/php/scripts/database.php
@@ -1,16 +1,31 @@
 <?php
 
 // création de la session
+// session_start();
+// $id_session = session_id();
+
+// connexion à la bdd
+// try {
+    // $host = '';
+    // $user = '';
+    // $password = '';
+
+    // $db = new PDO($host, $user, $password);
+    // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// } catch (PDOException $err) {
+    // die('Erreur de connexion: ' . $err->getMessage());
+// }
+
+
+
+// création de la session
 session_start();
 $id_session = session_id();
 
 // connexion à la bdd
 try {
-    $host = 'mysql:host=mysql-nasfernane.alwaysdata.net;dbname=nasfernane_discordphp';
-    $user = '216502_juliette';
-    $password = 'ducafésvp';
-
-    $db = new PDO($host, $user, $password);
+    $db = new PDO(getenv('DATABASE'), getenv('DATABASE_USER'), getenv('DATABASE_PASSWORD'));
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $err) {
