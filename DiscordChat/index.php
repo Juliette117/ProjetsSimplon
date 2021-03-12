a89cfe5baa12431ed8f9575130de64d7626218f0
Juliette117 <72933327+Juliette117@users.noreply.github.com>
2021-03-12 22:51:44 +0100

    Add files via upload

---
+++ DiscordChat/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'php/scripts/database.php';
+
+require './php/components/_header.php';
+
+$path = substr($_SERVER['REQUEST_URI'], 7);
+switch ($path) {
+    case 'login':
+        require 'php/views/login.php';
+        break;
+    case 'signup': 
+        require 'php/views/signup.php';
+        break;
+    default: 
+        require 'php/views/home.php';
+}
+
+require './php/components/_footer.php';
---
+++ DiscordChat/php/scripts/database.php
@@ -0,0 +1,30 @@
+<?php
+
+//création de la session
+session_start();
+$id_session = session_id();
+
+// //connexion à la bdd
+// try {
+    // $host = '';
+    // $user = '';
+    // $password = '';
+
+    // $db = new PDO($host, $user, $password);
+    // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// } catch (PDOException $err) {
+    // die('Erreur de connexion: ' . $err->getMessage());
+//    }
+
+// connexion à la bdd
+try {
+    $db = new PDO(getenv('DATABASE'), getenv('DATABASE_USER'), getenv('DATABASE_PASSWORD'));
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $err) {
+    die('Erreur de connexion: ' . $err->getMessage());
+   }
+
+
+?>
