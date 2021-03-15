ac496fec31a68830ccc835771e776868014620d4
Juliette117 <72933327+Juliette117@users.noreply.github.com>
2021-03-15 14:09:08 +0100

    Update logUser.php

--- DiscordChat/php/scripts/logUser.php
+++ DiscordChat/php/scripts/logUser.php
@@ -2,23 +2,25 @@
 
 require 'database.php';
 
+// Si les deux requêtes POST email et password existent
 if (isset($_POST['email']) && isset($_POST['password'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
-
+    //Récupération de l'utilisateur dans la BDD dont l'email correspond à la saisie
     $user = $db->prepare("
         SELECT * 
         FROM users
         WHERE email = '{$email}'
      ");
-
+    //Récupère tout les utilisateurs
     $user->execute();
     $user = $user->fetchAll();
 
-
+    //Si un utilisateur est trouvé 
     if ($user) {
+        //On récupère son ID
         $userId = $user[0]['idUser'];
-
+        //Changement du status en "en ligne" dans la bdd
         $userLogged = $db->prepare("
             UPDATE users
             SET isLogged='1'
@@ -28,12 +30,13 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
         $userLogged->execute();
 
 
-        // récupération du mot de passe de l'utilisateur enregistré, puis vérification du mot de passe entré
+        //Récupération du mot de passe de l'utilisateur enregistré, puis vérification du mot de passe entré
         $userPassword = $user[0]['password'];
         $isCorrectPw = password_verify($password, $userPassword);
 
-        // si les identifiants sont corrects, crée la session et ajoute son id avant de rediriger vers home
+        //Si les identifiants sont corrects
         if ($isCorrectPw) {
+            //Ouvrir la session et ajoute son id avant de rediriger vers home
             session_start();
             $id_session = session_id();
             $_SESSION['userid'] = $userId;
@@ -43,6 +46,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
             header('location: /?page=login');
             
         }
+        //Sinon, on redirige vers la page login
     } else {
         header('location: /?page=login');
         // echo "wrong user ";
